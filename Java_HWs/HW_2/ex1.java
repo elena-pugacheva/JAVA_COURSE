fe90e5dd10da89ac77e9d7e5a6b1acf46bea04ec
elenapugacheva <elenapugacheva2708@gmail.com>
2023-04-02 10:08:18 +0300

    hw2

--- Java_HWs/HW_2/ex1.java
+++ Java_HWs/HW_2/ex1.java
@@ -7,15 +7,32 @@
 // Используйте StringBuilder для подготовки ответа
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.*;
+
+
 public class ex1 {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws IOException {
       String jsonString = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
             "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}, " +
             "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
-      Parse(jsonString);
+      Logger logger = Logger.getLogger(ex1.class.getName());
+      logger.setLevel(Level.INFO);
+      FileHandler fileHandler = new FileHandler("log.txt");
+      logger.addHandler(fileHandler);
+      SimpleFormatter sFormat = new SimpleFormatter();
+      fileHandler.setFormatter(sFormat);
+      logger.log(Level.WARNING, "Тестовое логирование");
+      logger.info("Тестовое логирование");
+
+      String filePath = "students.txt";
+      String str = Parse(jsonString);
+      WriteToFile(str,filePath);
    }
 
-   public static void Parse(String jsonString) {
+   public static String Parse(String jsonString) {
       StringBuilder result = new StringBuilder();
       jsonString = jsonString.substring(1, jsonString.length() - 1);
       String[] objects = jsonString.split("},");
@@ -30,8 +47,33 @@ public class ex1 {
          result.append("Студент ").append(surname).append(" получил ").append(grade).append(" по предмету ")
                .append(subject).append(".\n");
       }
-      System.out.println(result);
-      ;
+      String str = result.toString();
+      return str;
    }
+   public static void WriteToFile(String s, String filePath){
+      File file = new File(filePath);
+      if (file.exists()) {
+         try {
+            file.createNewFile();
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+      }
 
+      try (FileWriter writer = new FileWriter(filePath, false)) {
+         writer.write(s);
+         Logger.getAnonymousLogger();
+         writer.flush();
+
+      } catch (Exception e) {
+         e.printStackTrace();
+         Logger.getAnonymousLogger();
+      }
+   }
 }
+
+
+
+
+
+
